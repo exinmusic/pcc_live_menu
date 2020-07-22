e3af2d54e7ea5420a706cbdc7d414d010b8b2483
exinmusic <exinmusic@gmail.com>
2020-07-22 10:13:18 -0700

    Basic Working Menu

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,61 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <table class="ui very basic celled table">
+      <thead>
+        <tr><th class="bump bump_down">Name</th>
+        <th class="bump_down">Phenotype</th>
+        <th class="bump_down">Company</th>
+        <th class="bump_down">THC</th>
+        <th class="bump_down">CBD</th>
+        <th class="bump_down">Gram</th>
+      </tr></thead>
+      <tbody v-for="item in info.data.results" :key="item.name">
+        <tr>
+          <td class="bump" data-label="Name">{{ item.name }}</td>
+          <td data-label="Phenotype">{{ item.phenotype }}</td>
+          <td data-label="Company">{{ item.company_name }}</td>
+          <td data-label="THC">{{ item.thc }}</td>
+          <td data-label="CBD">{{ item.cbd }}</td>
+          <td data-label="Gram">${{ item.price }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+    secret: String
+  },
+  methods: {
+    getData: function() {
+      axios
+      .get('https://www.portlandcannabis.com/api/flower/?limit=50')
+      .then(response => (this.info = response))  
+    }
+  },
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    this.getData();
+    this.intervalID = setInterval(this.getData.bind(this), 10000);
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.bump {
+  padding-left: 1em !important;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.bump_down {
+  padding-top: 1em !important;
 }
 </style>
